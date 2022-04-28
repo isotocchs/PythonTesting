00d31f2a2baf1eaf46cc104dd28baebf7bcc12e4
IgnacioSoto <isoto@columbushs.com>
2022-04-28 08:58:11 -0400

    save

--- PythonTesting/src/guiAdv.py
+++ PythonTesting/src/guiAdv.py
@@ -13,7 +13,10 @@ from kivy.properties import StringProperty, BooleanProperty
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.actionbar import ActionBar, ActionBarException, ActionButton, ActionItem, ActionView
-
+from kivy.graphics.vertex_instructions import Line
+from kivy.graphics.context_instructions import Color
+from kivy.graphics.vertex_instructions import Rectangle
+from kivy.metrics import dp
 
 #Define different Screens
 class HomeScreen(Screen):
@@ -36,6 +39,22 @@ class CanvasExample1(Widget):
 
 class CanvasExample2(Widget):
     pass
+
+class CanvasExample3(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas:
+            Line(points=(100,100,400,400), width=2)
+            Color(0,0,1)
+            Line(circle=(300,300,80), width=4)
+            Line(rectangle=(200,200,80,100), width=2)
+            self.rect = Rectangle(pos=(500,300),size=(50,100))
+    def on_button_press(self):
+        #self.rect.pos = (500,50)
+        x,y = self.rect.pos
+        x += dp(5)
+        self.rect.pos = (x,y)
+            
 
 
 class guiAdvKv(App):
